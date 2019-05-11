406de0b8ed3dbc778dafed049f8797fc5c205ee9
meredithcmiles <36449377+meredithcmiles@users.noreply.github.com>
2019-05-11 17:56:09 -0400

    Create bootstrapResid.R

---
+++ R/bootstrapResid.R
@@ -0,0 +1,22 @@
+# compute standardized residuals from 2 bootstrapped trait datasets
+# returns data in identical structure as binarize
+
+bootstrapResid<-function(y, x){
+  y<-y[complete.cases(y),]
+  x<-x[complete.cases(x),]
+  
+  x<-x[-which(rownames(x) %in% rownames(y) == FALSE),]
+  y<-y[-which(rownames(y) %in% rownames(x) == FALSE), ]
+  
+  out<-matrix(NA, nrow=nrow(y), ncol=ncol(y))
+  
+  for (i in 1:ncol(x)){
+    x1<-x[,i]
+    y1<-y[,i]
+    r<-rstandard(lm(y1~x1))
+    out[,i]<-r
+  }
+  
+  return(binarize(out))
+  
+}
