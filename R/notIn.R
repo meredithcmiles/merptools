8f7b38ae550d233bfd069822932d01336a8f24d2
meredithcmiles <36449377+meredithcmiles@users.noreply.github.com>
2019-05-13 08:36:31 -0400

    Update notIn.R

--- R/notIn.R
+++ R/notIn.R
@@ -1,5 +1,15 @@
 notIn<-function(x, y){
+  
+  if (isTree(x)==TRUE){
+    x<-x$tip.label
+  }
+  
+  if (isTree(y)==TRUE){
+    y<-y$tip.label
+  }
+  
   x_not_y<-x[which(x %in% y == FALSE)]
   y_not_y<-y[which(y %in% x == FALSE)]
+  
   return(list("x_not_y"=x_not_y, "y_not_x"=y_not_x))
 }
