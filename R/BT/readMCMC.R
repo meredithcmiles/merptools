920874aed015cf9a45c00a97b481af98c0c28611
meredithcmiles <36449377+meredithcmiles@users.noreply.github.com>
2019-05-22 10:42:55 -0400

    Create readMCMC.R

---
+++ R/BT/readMCMC.R
@@ -0,0 +1,16 @@
+readMCMC = function(file){
+  info<-getInfo(file)
+  skp<-startline(file)-1
+  
+  ratenames<-paste("q", info[,1], info[,2], sep="")
+  est<-which(!is.na(info[,3]))
+  parnames<-paste("q", info[est,1], info[est,2], sep="")
+  parloc<-matrix(c(info[est,1], info[est,2]), ncol=2)
+  
+  readin<-read.table(file, sep="\t", skip=skp, header=TRUE)
+  cols <- colnames(readin)
+  
+  readin<-readin[,which(cols %in% parnames==TRUE)]
+  
+  return(readin)
+}
